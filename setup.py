cc34effc006deb51b8dfa324c5213ad1d7285c51
Harshit161999 <43957138+Harshit161999@users.noreply.github.com>
2020-11-13 23:09:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
           'sys',
           'pandas',
           'numpy',
-          'math'
+          'math',
+          'pip'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
